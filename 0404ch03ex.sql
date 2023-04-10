1246a407f61eac4a36293246300a052a6cc0b28e
박현빈 <127382049+ppareu@users.noreply.github.com>
2023-04-10 12:20:17 +0900

    4월 10일 실습

--- 0404ch03ex.sql
+++ 0404ch03ex.sql
@@ -1,49 +1,60 @@
-/* 박지성이 구매한 도서의 출판사 수*/
-SELECT COUNT(publisher)
-FROM book, orders
-WHERE book.bookid = orders.bookid AND orders.custid = 1;
+/*01 - 5 박지성이 구매한 도서의 출판사 수*/
+SELECT COUNT(distinct publisher)
+FROM customer, book, orders
+WHERE customer.custid = orders.custid AND orders.bookid = book.bookid AND customer.name LIKE '박지성';
 
-/* 박지성이 구매한 도서의 이름, 가격, 정가와 판매가격의 차이 */
+/*01 - 6 박지성이 구매한 도서의 이름, 가격, 정가와 판매가격의 차이 */
 SELECT bookname, price, price-saleprice
-FROM book, orders, customer
-WHERE book.bookid = orders.bookid AND orders.custid = customer.custid AND customer.name = '박지성';
+FROM customer, book, orders
+WHERE customer.custid = orders.custid AND orders.bookid = book.bookid AND customer.name LIKE '박지성';
 
-/* 박지성이 구매하지 않은 도서의 이름 */
-SELECT DISTINCT BOOK.BOOKNAME
-FROM BOOK, ORDERS, CUSTOMER
-WHERE BOOK.BOOKID = ORDERS.BOOKID AND CUSTOMER.CUSTID = ORDERS.CUSTID AND CUSTOMER.NAME != '박지성';
+/*01 - 7 박지성이 구매하지 않은 도서의 이름 */
+SELECT bookname
+FROM book
+WHERE NOT EXISTS (
+    SELECT bookname
+    FROM customer, orders
+    WHERE customer.custid = orders.custid AND orders.bookid = book.bookid AND customer.name LIKE '박지성'
+);
 
-/* 주문하지 않은 고객의 이름(부속질의 사용) */
-SELECT NAME
-FROM CUSTOMER
-WHERE CUSTID NOT IN (SELECT CUSTID
-                        FROM ORDERS);
+/*02 - 8 주문하지 않은 고객의 이름(부속질의 사용) */
+SELECT name
+FROM customer
+WHERE name NOT IN ( 
+    SELECT name
+    FROM customer, orders
+    WHERE customer.custid=orders.custid
+);
 
-/* 주문 금액의 총액과 주문의 평균 금액 */
-SELECT SUM(SALEPRICE), AVG(SALEPRICE)
+/*02 - 9 주문 금액의 총액과 주문의 평균 금액 */
+SELECT SUM(saleprice), AVG(saleprice)
 FROM ORDERS;
 
-/* 고객의 이름과 고객별 구매액 */
-SELECT CUSTOMER.NAME, SUM(ORDERS.SALEPRICE)
-FROM CUSTOMER, ORDERS
-WHERE CUSTOMER.CUSTID = ORDERS.CUSTID
-GROUP BY CUSTOMER.NAME;
+/*02 - 10 고객의 이름과 고객별 구매액 */
+SELECT name, SUM(saleprice) AS total
+FROM customer, orders
+WHERE customer.custid = orders.custid
+GROUP BY name;
 
-/* 고객의 이름과 고객이 구매한 도서 목록 */
-SELECT CUSTOMER.NAME, BOOK.BOOKNAME
-FROM BOOK, CUSTOMER, ORDERS
-WHERE BOOK.BOOKID = ORDERS.BOOKID AND CUSTOMER.CUSTID = ORDERS.CUSTID;
+/*02 - 11 고객의 이름과 고객이 구매한 도서 목록 */
+SELECT name, book.bookname
+FROM customer, book, orders
+WHERE book.bookid = orders.bookid AND customer.custid = orders.custid;
 
-/* 도서의 가격(Book 테이블)과 판매가격(Orders 테이블)의 차이가 가장 많은 주문 */
-SELECT ORDERS.*
-FROM BOOK, ORDERS
-WHERE BOOK.BOOKID = ORDERS.BOOKID AND BOOK.PRICE - ORDERS.SALEPRICE = (SELECT MAX(BOOK.PRICE-ORDERS.SALEPRICE)
-FROM BOOK, ORDERS
-WHERE BOOK.BOOKID = ORDERS.BOOKID);
+/*02 - 12 도서의 가격(Book 테이블)과 판매가격(Orders 테이블)의 차이가 가장 많은 주문 */
+SELECT *
+FROM book, orders
+WHERE book.bookid = orders.bookid AND price - saleprice = (
+    SELECT MAX(price - saleprice)
+    FROM book, orders
+    WHERE book.bookid=orders.bookid
+);
 
-/* 도서의 판매액 평균보다 자신의 구매액 평균이 더 높은 고객의 이름 */
-SELECT NAME
-FROM CUSTOMER, ORDERS
-WHERE CUSTOMER.CUSTID = ORDERS.CUSTID
-GROUP BY NAME HAVING AVG(SALEPRICE) > (SELECT AVG(ORDERS.SALEPRICE)
-FROM ORDERS);
+/*02 - 13 도서의 판매액 평균보다 자신의 구매액 평균이 더 높은 고객의 이름 */
+SELECT name, avg(saleprice)
+FROM customer, orders
+WHERE customer.custid = orders.custid
+GROUP BY name HAVING avg(saleprice) > (
+    SELECT avg(saleprice)
+    FROM orders
+);
