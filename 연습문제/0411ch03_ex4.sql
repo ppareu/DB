1d8348db64541c6e8e60c193b6757f99a07f6995
박현빈 <127382049+ppareu@users.noreply.github.com>
2023-04-11 11:14:16 +0900

    Update 0411ch03_ex4.sql

--- 연습문제/0411ch03_ex4.sql
+++ 연습문제/0411ch03_ex4.sql
@@ -11,3 +11,12 @@ SELECT * FROM book;
 --(4)
 update book set publisher='대한출판사' where publisher like '대한미디어';
 SELECT * FROM book;
+
+--(5)
+CREATE TABLE Bookcompany (name VARCHAR(20) PRIMARY KEY, address VARCHAR(20), begin DATE);
+
+--(6)
+ALTER TABLE Bookcompany ADD webaddress VARCHAR(30);
+
+--(7)
+INSERT INTO Bookcompany VALUES (‘한빛아카데미’, ‘서울시 마포구’, 1993-01-01, ‘http://hanbit.co.kr’);
